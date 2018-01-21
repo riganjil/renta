fcc3566a0cd1ed7044813a2d308eb85fa2e02e51
arimaulana <ari.maulana08@gmail.com>
2018-01-21 22:34:34 +0700

    profile

--- resources/views/pages/account/index.blade.php
+++ resources/views/pages/account/index.blade.php
@@ -7,34 +7,56 @@
 
     <div class="row">
         <div class="col-md-12">
-            <h3 class="my-4">My Account</h3>
+            <h3 class="my-4">Akun Saya</h3>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-3">
+            <ul class="nav flex-column" id="nav_side">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Ubah profil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Poin</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pesanan</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Keranjang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pembelian</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Keranjang</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-9">
             <form action="">
                 <div class="form-group has-success">
-                    <label class="form-control-label" for="inputSuccess1">Email</label>
-                    <input type="text" class="form-control form-control-success" id="inputSuccess1">
+                    <label class="form-control-label" for="">Nama Lengkap</label>
+                    <input type="text" class="form-control form-control-success" value="{{Auth::User()->nama_depan.' '.Auth::User()->nama_belakang}}" readonly>
                     <small class="form-text text-muted">Example help text that remains unchanged.</small>
                 </div>
                 <div class="form-group has-success">
-                    <label class="form-control-label" for="inputSuccess1">Phone</label>
-                    <input type="text" class="form-control form-control-success" id="inputSuccess1">
+                    <label class="form-control-label" for="">Phone</label>
+                    <input type="text" class="form-control form-control-success" value="{{Auth::User()->telepon}}"  readonly>
                     <small class="form-text text-muted">Example help text that remains unchanged.</small>
                 </div>
                 <div class="form-group has-success">
-                    <label class="form-control-label" for="inputSuccess1">Address</label>
-                    <input type="text" class="form-control form-control-success" id="inputSuccess1">
+                    <label class="form-control-label" for="">Email</label>
+                    <input type="text" class="form-control form-control-success" value="{{Auth::User()->email}}"  readonly>
                     <small class="form-text text-muted">Example help text that remains unchanged.</small>
                 </div>
                 <div class="form-group has-success">
-                    <label class="form-control-label" for="inputSuccess1">Message</label>
-                    <textarea class="form-control"></textarea>
+                    <label class="form-control-label" for="">Username</label>
+                    <input type="text" class="form-control form-control-success" value="{{Auth::User()->username}}"  readonly>
                     <small class="form-text text-muted">Example help text that remains unchanged.</small>
                 </div>
                 <div class="form-group has-success">
-                    <button class="btn btn-primary">Send</button>
+                    <button class="btn btn-primary">Ubah</button>
                 </div>
             </form>
         </div>
